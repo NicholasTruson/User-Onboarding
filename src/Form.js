dc30588abd87a7d4c0a65658b65d60a4ce79e7c7
Nicholas Truson <nicholas.truson@gmail.com>
2019-08-22 12:18:56 -0700

    imports, base user for, with formik touched errors

---
+++ src/Form.js
@@ -0,0 +1,53 @@
+// *** IMPORTS *** //
+
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Form, Field, withFormik } from 'formik';
+import * as Yup from 'yup';
+
+// *** USER FORM *** //
+
+const UserForm = ({ errors, touched, values, status }) => {
+    const [users, setUsers] = useState([]);
+    console.log(users);
+
+    useEffect(() => {
+        if (status) {
+          setUsers([...users, status]);
+        }
+      }, [status]);
+
+    return (
+        <div className="userForm">
+            <h1>Welcome!</h1>
+            <Form>
+                <Field type="text" name="username" placeholder="Enter Username"/>
+                {touched.username && errors.username && (
+                    <p classname="error">{errors.username}</p>
+                )}
+
+                <Field type="text" name="email" placeholder="Enter Email"/>
+                {touched.email && errors.email && (
+                    <p classname="error">{errors.email}</p>
+                )}
+                <Field type="password" name="password" placeholder="Enter Secure Password"/>
+                {touched.password && errors.password && (
+                    <p classname="error">{errors.password}</p>
+                )}
+
+
+                <Field type="checkbox" name="agree to terms" />
+
+
+                <Field type="text" name="agree to terms" />
+                {touched.email && errors.email && (
+                    <p classname="error">{errors.email}</p>
+                )}
+
+                <button type="submit">Submit!</button>
+            </Form>
+        </div>
+    )
+}
+
+// *** FORMIK - USER FROM *** //
